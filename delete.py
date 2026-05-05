e0aa160013a94379d8ce775ebe6bbb813f28dff2
marlonandressilvagarcia-alt <marlonandressilvagarcia@gmail.com>
2026-05-05 14:31:29 -0500

    feat: agregar codigo funcion eliminar servicios

---
+++ delete.py
@@ -0,0 +1,28 @@
+def eliminar_servicio():
+    servicios = cargar_servicios()
+
+    if not servicios:
+        print("No hay servicios para eliminar.\n")
+        return
+
+    print("=== Lista de Servicios ===")
+    for i, s in enumerate(servicios):
+        print(f"{i + 1}. {s['nombre']} - {s['tipo_evento']}")
+
+    try:
+        opcion = int(input("Seleccione el número del servicio a eliminar: ")) - 1
+        if opcion < 0 or opcion >= len(servicios):
+            print("Opción inválida.\n")
+            return
+    except ValueError:
+        print("Debe ingresar un número.\n")
+        return
+
+    confirmacion = input("¿Seguro que quieres eliminar este servicio? (Si/No): ").capitalize()
+
+    if confirmacion == "Si":
+        eliminado = servicios.pop(opcion)
+        guardar_servicios(servicios)
+        print(f"✔ Servicio '{eliminado['nombre']}' eliminado.\n")
+    else:
+        print("Operación cancelada.\n")
